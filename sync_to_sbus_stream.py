087318330db84f25087d3397e97790480b1bdf0b
Samuel Pontefract <142825916+Turbofan3360@users.noreply.github.com>
2024-09-21 13:12:21 +0200

    Update sync_to_sbus_stream.py

--- sync_to_sbus_stream.py
+++ sync_to_sbus_stream.py
@@ -1,34 +1,39 @@
 from machine import UART
+import time
 
-class sbus_receive:
+class SbusReceive:
+    
+    TIMEOUT_PERIODS = 26
+    
     def __init__(self, port):
         self.sbus = UART(port, 100000)
         self.sbus.init(100000, bits=8, parity=0, stop=2, invert=self.sbus.INV_RX)
         
         self.data = bytearray(23)
-        self.testBytes = bytearray(2)
-        self.timeout_flag = 0
-        
-    def read_data(self):
-        while self.timeout_flag < 26:
-            self.timeout_flag += 1
-            print("Searching...")
-            self.testBytes = self.sbus.read(2)
             
-            if self.testBytes == b'\x00\x0f':
-                print("Found stop and start byte...")
-                
-                while self.sbus.any() < 23:
+    def read_data(self):
+        timeout_flag = 0
+        
+        while timeout_flag < SbusReceive.TIMEOUT_PERIODS:
+            timeout_flag += 1
+            testbytes = self.sbus.read(2)
+            
+            if testbytes == b'\x00\x0f':
+                time.sleep_ms(50)
+                if self.sbus.any() < 23:
                     pass
                 
                 self.data = self.sbus.read(23)
                 return self.data
-        
         return None
 
-sbus = sbus_receive(1)
-data = sbus.read_data()
-if data != None:
-    print(data)
-else:
-    print("No data")
+
+sbus = SbusReceive(1)
+
+while True:
+    data = sbus.read_data()
+    
+    if data == None:
+        print("No data")
+    else:
+        print(data)
