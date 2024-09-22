8ffe182cd8a348fdf4559588aa1f026602d1f41e
Samuel Pontefract <142825916+Turbofan3360@users.noreply.github.com>
2024-09-22 17:58:48 +0200

    Update sbus_decoding_channels.py

--- SBUS/sbus_decoding_channels.py
+++ SBUS/sbus_decoding_channels.py
@@ -9,9 +9,8 @@ class SbusReceive:
         self.sbus = UART(port, 100000)
         self.sbus.init(100000, bits=8, parity=0, stop=2, invert=self.sbus.INV_RX)
         
-        self.data = bytearray(23)
-        self.previousData = bytearray(23)
-        self.dataDuplicate = False
+        self.previousData = bytearray()
+        self.dataDuplicated = False
         self.channels = 16*[0]
             
     def read_data(self):
@@ -22,24 +21,25 @@ class SbusReceive:
             testbytes = self.sbus.read(2)
             
             if testbytes == b'\x00\x0f':
-                time.sleep_ms(50)
-                if self.sbus.any() < 23:
-                    pass
+                self.data = bytearray()
                 
-                self.data = self.sbus.read(23)
+                while len(self.data) < 23:
+                    nextbytes = self.sbus.read(23 - len(self.data))
+                    if nextbytes:
+                        self.data += nextbytes
                 
                 if self.data == self.previousData:
-                    self.dataDuplicate = True
+                    self.dataDuplicated = True
                 else:
-                    self.dataDuplicate = False
+                    self.dataDuplicated = False
                     self.previousData = self.data
                 
                 return self.data
         return None
 
     def extract_channel_data(self):
-        if self.dataDuplicate:
-            return "Duplicate"
+        if self.dataDuplicated:
+            return
         
         channels = 16*[0]
         byte_in_sbus = 0
@@ -63,7 +63,7 @@ class SbusReceive:
                 ch += 1
             
         self.channels = channels
-        return channels
+        return channels[:8]
 
 
 sbus = SbusReceive(1)
@@ -71,8 +71,9 @@ sbus = SbusReceive(1)
 while True:
     data = sbus.read_data()
     
-    if data == None:
-        time.sleep_ms(100)
-    else:
+    if data:
         channels = sbus.extract_channel_data()
-        print(channels)
+        if channels:
+            print(channels)
+    else:
+        time.sleep_ms(5)
